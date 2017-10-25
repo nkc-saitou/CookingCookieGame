d6624104da5a496c878c0c473071301237e518ea
FunabikiHayato <suralin1236@gmail.com>
2017-10-25 14:55:17 +0900

    no message

---
+++ Assets/Script/title.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title : MonoBehaviour {
+
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
