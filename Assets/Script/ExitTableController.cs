f559b59d70ac9cdfa16ee6a4f208fd92288986eb
DESKTOP-A792KI4\user <nkc.yuki.saitou@gmail.com>
2017-11-01 13:51:36 +0900

    味方クッキーを出す

---
+++ Assets/Script/ExitTableController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//===================================================
+// 出口オブジェクト
+//===================================================
+[System.Serializable]
+public class DirectionName
+{
+    [SerializeField]
+    string name = "東";
+    
+    [Header("工場内のオブジェクト")]
+    public GameObject destroyObj;
+
+    [Header("外のオブジェクト")]
+    public GameObject createObj;
+
+    [System.NonSerialized]
+    public bool createOKflg = false;
+
+    public enum DirName
+    {
+        North = 0, //北
+        South,     //南
+        East,      //東
+        West       //西
+    }
+
+    [Header("どの方向に置かれているのか")]
+    public DirName dirName;
+}
+
+public class ExitTableController : MonoBehaviour
+{
+    public GameObject cookieSoldierPre; //作ったクッキーのPrefab
+    public DirectionName[] directionName;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        foreach(DirectionName n in directionName)
+        {
+            //フラグをリセット
+            if(n.destroyObj.transform.childCount == 0)
+            {
+                n.createOKflg = true;
+            }
+            //子オブジェクトがある状態で、フラグがリセットされていたら
+            else if (n.destroyObj.transform.childCount != 0 && n.createOKflg == true)
+            {
+                //中で作ったクッキーを削除
+                Destroy(n.destroyObj.transform.GetChild(0).gameObject);
+
+                //外にクッキーを出す
+                Instantiate(
+                    cookieSoldierPre, 
+                    directionName[(int)n.dirName].createObj.transform.localPosition, 
+                    Quaternion.identity,directionName[(int)n.dirName].createObj.transform);
+
+                n.createOKflg = false;
+            }
+        }
+    }
+}
