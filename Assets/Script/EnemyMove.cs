5d6032a7715a303a7bf290cd780e15dfe79d08a2
Takagi <nkc.ryousuke.takagi@gmail.com>
2017-11-01 13:46:47 +0900

    敵の沸き、移動

---
+++ Assets/Script/EnemyMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+    //工場の入口前
+    public Vector2 pos;
+    public Vector2 FactryGate;
+    //速さ
+    public Vector2 speed=new Vector2(0.05f,0.05f);
+    // ラジアン
+    private float rad;
+    // 現在位置
+    private Vector2 Position;
+    void Start () {
+    }
+	
+	void Update () {
+        if (pos.x-0.5f<=gameObject.transform.position.x&&gameObject.transform.position.x<= pos.x+0.5f
+            || pos.y-0.5f<=transform.position.y&&transform.position.y<=pos.y+0.5f)
+        {
+            
+            Debug.Log(pos);
+        }
+        rad = Mathf.Atan2(
+            pos.x - transform.position.y,
+            pos.y - transform.position.x);
+        Position = transform.position;
+        Position.x += speed.x * Mathf.Cos(rad);
+        Position.y += speed.y * Mathf.Sin(rad);
+        transform.position = Position;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "Gate")
+        {
+            pos.x = FactryGate.x;
+            pos.y = FactryGate.y;
+        }
+    }
+}
---
+++ Assets/Script/EnemySpawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject Cookie1;
+    private int Pattern;
+    private int a;
+	void Start () {
+        StartCoroutine("ESpawn");
+	}
+	
+
+	void Update () {
+	}
+
+    private void RandomSpawnECookie()
+    {
+        RandomPM();
+        switch (Pattern = Random.Range(1, 3))
+        {
+            case 1://上下のクッキーの沸き
+                GameObject Cookie = Instantiate(Cookie1, new Vector3(Random.Range(-5.5f, 5.5f), 5 * a, 0), Quaternion.identity);
+                EnemyMove EM = Cookie.GetComponent<EnemyMove>();
+                EM.pos = transform.position;
+                EM.pos.x = 3.5f * a; EM.pos.y = 0;
+                EM.FactryGate.x = 2 * a; EM.FactryGate.y = 0;
+                Debug.Log(EM.pos);
+                break;
+
+            case 2://左右のクッキーの沸き
+                GameObject cooKie = Instantiate(Cookie1, new Vector3(9 * a, Random.Range(-4f, 4f), 0), Quaternion.identity);
+                EnemyMove _EM = cooKie.GetComponent<EnemyMove>();
+                _EM.pos = transform.position;
+                _EM.pos.x = 0; _EM.pos.y = 4.5f * a;
+                _EM.FactryGate.x = 0; _EM.FactryGate.y = 3 * a;
+                Debug.Log(_EM.pos);
+                break;
+        }
+    }
+
+    private IEnumerator ESpawn()//敵の沸き間隔
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2.0f);
+            RandomSpawnECookie();
+        }
+    }
+
+    private void RandomPM()//正負のランダム
+    {
+        a = Random.Range(0, 2);
+        a *= 2;
+        a--;
+    }
+}
